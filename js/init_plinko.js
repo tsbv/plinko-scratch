885a63fd7a6b3df293577fe487d0f8238798a288
Georgyi <112905032+georgij-tsabiev@users.noreply.github.com>
2025-07-09 16:06:49 +0300

    final

--- js/init_plinko.js
+++ js/init_plinko.js
@@ -1,3 +1,10 @@
+// Настройки бонуса
+var MAX_BONUS = 1000; // Максимальный бонус в €
+var BONUS_MULTIPLIER = 3; // Коэффициент умножения базового бонуса
+// При MAX_BONUS = 500 и BONUS_MULTIPLIER = 2: базовый бонус 0-250€
+// При MAX_BONUS = 50000 и BONUS_MULTIPLIER = 2: базовый бонус 0-25000€
+// При MAX_BONUS = 50000 и BONUS_MULTIPLIER = 200: базовый бонус 0-250€
+
 var bonusUpdateInt = setInterval(bUpdate, 50);
 var curBonus = 0;
 var realBonus = 0;
@@ -8,7 +15,8 @@ var canStartGame = false;
 function bUpdate() {
     if (curBonus > realBonus) curBonus = realBonus;
     if (curBonus < realBonus) {
-        curBonus += 5;
+        curBonus += Math.max(100, Math.floor((realBonus - curBonus) / 5));
+        if (curBonus > realBonus) curBonus = realBonus;
         var div = document.getElementById('bonus');
         div.innerHTML = '<span class="nl-block none">WIN €' + curBonus + ' BONUS</span><span class="fr-block none">WIN €' + curBonus + ' BONUS</span>';
     }
@@ -20,7 +28,10 @@ playBtn.onclick = function() {
     startPlinko(5, 3);
 };
 function updateBonus(bonusSum) {
-    realBonus = bonusSum;
+    realBonus = bonusSum * BONUS_MULTIPLIER;
+    if (realBonus > MAX_BONUS) {
+        realBonus = MAX_BONUS;
+    }
 }
 function gameOver() {
     setTimeout(() => {
@@ -40,3 +51,20 @@ $(".close--reg").on('click', function() {
     div.innerHTML = '<span class="nl-block none">WIN €' + curBonus + ' BONUS</span><span class="fr-block none">GAGNER €' + curBonus + ' BONUS</span>';
     canStartGame = true;
 });
+
+function resetGame() {
+    var playBtn = document.getElementById("playbtn");
+    window.maxRound = 3;
+    for (let i = particles.length - 1; i >= 0; i--) {
+        World.remove(world, particles[i].body);
+        particles.splice(i, 1);
+    }
+    curBonus = 0;
+    realBonus = 0;
+    bonus = 0;
+    updateBonus(bonus);
+    window.plinkoBalls = 0;
+    window.plinkoBallsNow = 0;
+    playBtnEnabled = true;
+    playBtn.classList.remove("playbtn-disable");
+}
